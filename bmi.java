bc6b8c334edfe13a1006babe17a53aa13068a2b1
Colossus92 <calvinrichardson@gmail.com>
2018-06-14 21:12:19 +0200

    add new bmi

---
+++ bmi.java
@@ -0,0 +1,9 @@
+
+public class bmi{
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+		System.out.println("working file");
+		System.out.println("Commit directly");
+    }
+}
